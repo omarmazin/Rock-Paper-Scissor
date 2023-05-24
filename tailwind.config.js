338b6893150edf019b1c16cc46f70f1650b6649e
omarmazin <omr.ad@outlook.com>
2023-05-25 02:47:18 +0300

    changing index file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html", "./dist/output.css"],
+  content: ["./index.html", "./dist/output.css"],
   theme: {
     extend: {},
   },
