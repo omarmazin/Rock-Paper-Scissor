d9ddd5c9fad13bb06ed086d548e82d90b0a91096
omarmazin <omr.ad@outlook.com>
2023-05-24 23:11:03 +0300

    e

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: [ './src/*.html',
+  './src/**/*.{js,jsx,ts,tsx}',],
   theme: {
     extend: {},
   },
